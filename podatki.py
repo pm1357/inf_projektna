ff37422b2443334380642312c36c46fe04175cca
pm1357 <105114829+pm1357@users.noreply.github.com>
2022-05-31 22:29:57 +0200

    Delete podatki.py

--- podatki.py
+++
@@ -1,8 +0,0 @@
-class podatki:
-    koeficient_beli = None
-    koeficient_crni = None
-    raiting_beli = None
-    raiting_crni = None
-    rezultat = None
-    novi_raiting_beli = None
-    novi_raiting_crni = None
